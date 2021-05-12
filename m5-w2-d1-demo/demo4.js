8449351242bf24b67e65df63000c8ea736a5d554
Jerusalem Darkera <jdarkera@gmail.com>
2021-05-11 20:14:42 -0700

    Commit #2

--- m5-w2-d1-demo/demo4.js
+++ m5-w2-d1-demo/demo4.js
@@ -1,11 +1,12 @@
   class NameForm extends React.Component {
     constructor(props) {
       super(props);
-      
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.input = React.createRef();
     }
-  
     handleSubmit(event) {
-      
+      alert('A name was submitte:' + this.input.current.value);
+      event.preventDefault();
     }
   
     render() {
@@ -13,7 +14,7 @@
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text"  />
+            <input type="text" ref={this.input}  />
           </label>
           <input type="submit" value="Submit" />
         </form>
